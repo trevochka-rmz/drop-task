8c5f78e29f2b5adc22d1471f6ab9c1460a481d48
tr3vochka-rmz <kanalmoveron@gmail.com>
2025-05-16 19:21:21 +0700

    Обновлен api-url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://drop-task-backend.onrender.com/api';
 
 // Логирование запросов
 const logRequest = (method, endpoint, data) => {
